bcde05c0441d357411aeaf9b01509be6deba7f6d
Oll-E-the-first <alfoktro@gmail.com>
2018-06-12 14:12:09 +0200

    redesign

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var kirschencount = 0;
 
+function init(){
+  kirschencount = prompt("Wieviele Kirschen haben Sie schon gepflueckt?");
+  kirschencount = Number(kirschencount);
+  if (isNaN(kirschencount)) {
+    kirschencount = 0;
+    return;
+  }
+}
+
 function kirschenpfluecken() {
   var temp = Math.floor(Math.random()*9+1);
   alert("Kirschen wurden gepflueckt");
   alert(temp+0);
-  kirschencount = kirschencount+temp;
+  kirschencount = Number(kirschencount)+Number(temp);
 }
 
 function displayKirschen() {
